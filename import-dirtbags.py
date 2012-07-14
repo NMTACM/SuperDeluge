0c9593405560aee4cd6c7a631cc2f6d54498f611
Chris Cowan <agentme49@gmail.com>
2012-07-14 06:06:40 -0600

    Script for reading dirtbags puzzles.

---
+++ import-dirtbags.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+def read_puzzles(path):
+    for category in os.listdir(path):
+        catpath = os.path.join(path, category)
+        if os.path.isdir(catpath):
+            for value in os.listdir(catpath):
+                valpath = os.path.join(catpath, value)
+                if os.path.isdir(valpath):
+                    olddir = os.getcwd()
+                    os.chdir(valpath)
+
+                    puzzle = dict()
+
+                    puzzle['category'] = category
+
+                    if os.path.exists("@points.txt"):
+                        with file("@points.txt") as f:
+                            puzzle['value'] = int(f.read())
+                    else:
+                        puzzle['value'] = int(value)
+
+                    if os.path.exists("@author.txt"):
+                        with file("@author.txt") as f:
+                            puzzle['author'] = f.read().strip()
+                    else:
+                        puzzle['author'] = None
+
+                    if os.path.exists("@summary.txt"):
+                        with file("@summary.txt") as f:
+                            puzzle['notes'] = f.read().strip()
+                    else:
+                        puzzle['notes'] = None
+
+                    if os.path.exists("@index.mdwn"):
+                        with file("@index.mdwn") as f:
+                            puzzle['description'] = f.read().strip()
+                    else:
+                        puzzle['description'] = None
+
+                    with file("@answer.txt") as f:
+                        puzzle['answer'] = f.read().strip()
+
+                    puzzle['files'] = list()
+                    for filename in os.listdir("."):
+                        if filename[0] == "@":
+                            continue
+                        pfile = (filename, os.path.join(valpath, filename))
+                        puzzle['files'].append(pfile)
+
+                    os.chdir(olddir)
+                    yield puzzle
+
+
+if __name__=="__main__":
+    for puzzle in read_puzzles(sys.argv[1]):
+        print puzzle
